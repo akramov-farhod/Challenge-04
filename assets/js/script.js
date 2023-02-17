ef89cc8b5c18bf7acf7cf365a17183e9f0961fc6
Farhod Akramov <farhod@Farhods-MacBook-Air.local>
2023-02-17 01:10:20 -0700

    i think i have finished with the general layour, all i have left is to add event listeners and dynamic data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,20 @@
-console.log("Script is Linked");
+// const main = document.querySelector("#mainContainer");
+// const h1 = document.createElement("h1");
+// const h2 = document.createElement("h2");
+// const div = document.createElement("div");
+// const button = document.createElement("button");
+// const ul = document.createElement("ul");
+// const li = document.createElement("li");
+
+// var answers = function () {
+//   for (let i = 0; i < 4; i++) {
+//     ul.append(li);
+//   }
+// };
+
+// h1.innerText = "Coding Quiz";
+
+// main.append(h1);
+// main.append(div);
+// main.append(ul);
+// answers();
